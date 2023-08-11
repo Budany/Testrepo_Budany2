134cd4a57a16b149de33dad8b022aa93c6a0e6e9
Budany <141141699+Budany@users.noreply.github.com>
2023-08-11 22:43:45 +0800

    Create Day_5.py

---
+++ Day_5.py
@@ -0,0 +1,5 @@
+列表推导(list comprehension)，new_list = [expression for item in iterable]
+返回一个list
+
+生成器表达式(generator expression), generator = (expression for item in iterable)
+大量数据的时候，用于节省内存，或是用于生成处理无限序列
